6674ac1c1ce3b9f3df4f52a90b1e1152c1a5d16d
Timur Agliev <timur@agliev.ru>
2020-01-19 00:38:47 +0300

    Изменил адрес базы данных под сервер

--- Farm/config.py
+++ Farm/config.py
@@ -1,5 +1,5 @@
 TOKEN = '918293759:AAHes4v1RqLSDsYkrdQyO-w8r6LKzJI3kmc'
-DATA = '/Users/timuragliev/Desktop/database.db'
+DATA = '/usr/local/bin/bot/My_farm/Farm/database.db'
 MARKET_PRICE = {
 	'egg' : 4,
 	'wool' : 25,
